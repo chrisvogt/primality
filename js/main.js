2e4b94cba12b8a50980b7fee2c519ceeb61e611c
Chris Vogt <mail@chrisvogt.me>
2015-04-14 22:20:03 +0000

    Initial commit of primality experiment.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function isPrime(num) {
+    if (num < 2) return false;
+    for (var i = 2; i < num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+function buildTable(floor, ceiling) {
+    for (var i = floor; i < ceiling; i++) {
+        var container = document.getElementById("num-table");
+        var content = document.createElement("div");
+        content.setAttribute('class', 'col-mb-2 col-3 col-dt-3');
+        content.innerHTML = '<p>' + i + '</p>';
+        container.appendChild(content);
+    }
+}
+buildTable(0, 50);
